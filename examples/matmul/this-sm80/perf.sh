5b4dab5a5dc2b5afe208428646268904d55e9672
Kuangjux <kuangjux@outlook.com>
2023-12-24 14:15:55 +0800

    Add perf docs.

--- examples/matmul/this-sm80/perf.sh
+++ examples/matmul/this-sm80/perf.sh
@@ -1,3 +1,6 @@
-nvcc -arch=sm_80 -Xcompiler -fopenmp matmul-v00.cu main.cu -o test && ./test 
-
+echo "Test Naive WMMA GEMM Performance."
+nvcc -arch=sm_80 -Xcompiler -fopenmp matmul-v00.cu main.cu -o test && ./test
+echo "Test CuTe-v1 GEMM Performance."
+nvcc -arch=sm_80 -Xcompiler -fopenmp matmul-v16.cu -o test -I../../../3rdparty/cutlass/include/ -I../../../3rdparty/cutlass/tools/util/include/  && ./test
+echo "Test CuTe-v2 GEMM Performance."
 nvcc -arch=sm_80 -Xcompiler -fopenmp matmul-v17.cu main.cu -o test -I ../../../3rdparty/cutlass/include/ && ./test stages 4
